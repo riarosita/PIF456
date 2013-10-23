a1622085d913fe8fc63e0715a539d34d005262a3
Ria Rosita Lestari <simpulsenyum@gmail.com>
2013-10-23 20:20:04 +0700

    Modul 3
    Cookie dan Session

---
+++ hapus_session.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hapus Session</title>
+</head>
+
+<body>
+<?
+//inisialisasi data session
+session_start();
+
+//set session jika belum ada
+if (isset($_SESSION['test'])) {
+	//hapus session test
+	unset($_SESSION['test']);
+	
+	echo 'session dihapus';
+}else {
+	echo 'unset';
+	
+	//mencetak semua elemen session
+	print_r($_SESSION);
+}
+
+?>
+
+<p>
+Tekan Refresh (F5);
+</body>
+</html>
